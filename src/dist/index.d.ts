f7021bf2a95c5878e9f8110f9f67aaea839a9751
poold3 <dallinpool@gmail.com>
2025-07-27 13:37:04 -0400

    Adding index types.

---
+++ src/dist/index.d.ts
@@ -0,0 +1,6 @@
+export * from "./component.js";
+export * from "./observable.js";
+export * from "./router.js";
+export * from "./state.js";
+export * from "./subject.js";
+export * from "./value.js";
