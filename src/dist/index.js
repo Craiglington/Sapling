2313559011f4c408261eedb015940194dc14a467
poold3 <dallinpool@gmail.com>
2026-09-13 20:34:23 -0400

    Configuring repo.

--- src/dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./component.js";
-export * from "./observable.js";
-export * from "./router.js";
-export * from "./state.js";
-export * from "./subject.js";
-export * from "./value.js";
